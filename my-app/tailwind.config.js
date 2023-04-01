03e0d323c3fa5ce26648ced50272de8c96f8f9b6
LastToKnow <burydima28122003@gmail.com>
2023-04-01 15:41:29 +0300

    chore: setting tailwind

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,9 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require(`tailwindcss/defaultTheme`)
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [ "./src/**/*.{js,ts,jsx,tsx}", ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Fira Sans', ...fontFamily.sans],
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            a: {
+              color: theme('colors.current'),
+            },
+          },
+        },
+      }),
+    },
+    colors: {
+      inherit: 'inherit',
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#000000',
+      white: '#ffffff',
+      gray: colors.slate,
+      primary: colors.indigo,
+      secondary: colors.rose,
+      tertiary: colors.emerald,
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 }
 
